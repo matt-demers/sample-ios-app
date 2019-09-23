adc7c41497def385d76ba74aef5bb78dd367b9fd
Matt Demers <matt.demers@appian.com>
2019-09-23 16:48:51 -0400

    Initial Commit

---
+++ sample-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-app
+//
+//  Created by Matt Demers on 9/23/19.
+//  Copyright © 2019 Matt Demers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
